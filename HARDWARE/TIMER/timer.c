665c204fd40bb26bd7010b585972d2f554e6eff2
hpz <1090377416@qq.com>
2019-10-02 17:39:27 +0800

    错误可能在motor[]没有初始化

--- HARDWARE/TIMER/timer.c
+++ HARDWARE/TIMER/timer.c
@@ -44,7 +44,7 @@ void TIM2_Int_Init(u16 arr,u16 psc)
     TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE ); //使能指定的TIM2中断,允许更新中断
 
     NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;  //TIM2中断
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;  //先占优先级0级
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  //先占优先级0级
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;  //从优先级3级
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道被使能
     NVIC_Init(&NVIC_InitStructure);  //根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器
